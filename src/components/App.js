ae9b7a406df9cbe4ec81bffb8a1c959078a0b358
sbaldwin24 <sterling.baldwin@gmail.com>
2018-02-25 15:48:12 -0600

    refactored using react-create-app

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Nav from "./Nav";
+import Home from "./Home";
+import Spinner from "./Spinner";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+
+class DynamicImport extends React.Component {
+	state = {
+		component: null
+	};
+
+	componentWillMount() {
+		this.props.load().then(mod =>
+			this.setState(() => ({
+				component: mod.default
+			}))
+		);
+	}
+
+	render() {
+		return this.props.children(this.state.component);
+	}
+}
+
+class App extends React.Component {
+	state = {
+		loading: true
+	};
+
+	componentDidMount() {
+		setTimeout(() => this.setState({ loading: false }), 1000);
+	}
+
+	render() {
+		const { loading } = this.state;
+		if (loading === true) {
+			return <Spinner />;
+		}
+		return (
+			<Router>
+				<div className="container">
+					<Nav />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route render={() => <p>Not Found</p>} />
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
+}
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+/**
+ * TODO
+ * Create Products && ShoppingCart Component
+ */
+function Nav() {
+  return (
+    <ul className="nav">
+      <li>
+        <NavLink exact activeClassName="active" to="/">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink exact activeClassName="active" to="/Products">
+          Products
+        </NavLink>
+      </li>
+      <li>
+        <NavLink exact activeClassName="active" to="/shopping-cart">
+          Shopping Cart
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
+
+export default Nav;
